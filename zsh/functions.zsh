f23574c6cf49dcfd76d627b71e28a1f2bf05e767
Matthis Lemaitre-Cosquer <yabesmirax@gmail.com>
2016-01-31 22:39:33 +0100

    Zsh - Added gitclean function
    From https://gist.github.com/ericelliott/2271bd12736585175c85

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -21,3 +21,8 @@ function extract () {
 		echo "'$1' is not a valid file"
 	fi
 }
+
+function gitclean () {
+  git remote prune origin
+  git branch -r --merged master | egrep -iv '(master|develop)' | sed 's/origin\///g' | xargs -n 1 git push --delete origin
+}
