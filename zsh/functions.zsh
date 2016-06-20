6598dbbfa53ec6c87b2f46f9df7798b5539a3374
Matthis LEMAITRE-COSQUER <matthis.lemaitre-cosquer@niji.fr>
2016-06-20 16:59:30 +0200

    Zsh - Fix parse error in transfer fx

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -27,5 +27,22 @@ function gitclean () {
   git branch -r --merged master | egrep -iv '(master|develop)' | sed 's/origin\///g' | xargs -n 1 git push --delete origin
 }
 
-transfer() { if [ $# -eq 0 ]; then echo "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md"; return 1; fi tmpfile=$( mktemp -t transferXXX ); if tty -s; then basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g'); curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile; else curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile ; fi; cat $tmpfile; rm -f $tmpfile; }
+transfer() {
+    if [ $# -eq 0 ]; then
+        echo "No arguments specified. Usage:\necho transfer /tmp/test.md\ncat /tmp/test.md | transfer test.md";
+        return 1;
+    fi
+
+    tmpfile=$( mktemp -t transferXXX );
+
+    if tty -s; then
+        basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g');
+        curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile;
+    else
+        curl --progress-bar --upload-file "-" "https://transfer.sh/$1" >> $tmpfile;
+    fi;
+
+    cat $tmpfile;
+    rm -f $tmpfile;
+}
 
