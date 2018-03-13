06d2197c77443c9b66ec5a210410b9563eca4c34
Matthis LEMAITRE-COSQUER <matthis.lemaitre-cosquer@niji.fr>
2018-03-13 09:43:52 +0100

    feat(aliases): do some cleanup

--- git/git-aliases.zsh
+++ git/git-aliases.zsh
@@ -12,23 +12,13 @@ alias gbda='git branch --merged | command grep -vE "^(\*|\s*master\s*$)" | comma
 alias gbl='git blame -b -w'
 alias gbnm='git branch --no-merged'
 alias gbr='git branch --remote'
-alias gbs='git bisect'
-alias gbsb='git bisect bad'
-alias gbsg='git bisect good'
-alias gbsr='git bisect reset'
-alias gbss='git bisect start'
 
 alias gc='git commit -v'
-alias gc!='git commit -v --amend'
-alias gca='git commit -v -a'
-alias gca!='git commit -v -a --amend'
-alias gcan!='git commit -v -a -s --no-edit --amend'
 alias gcb='git checkout -b'
 alias gcf='git config --list'
 alias gcl='git clone --recursive'
 alias gclean='git reset --hard && git clean -dfx'
 alias gcm='git checkout master'
-alias gcmsg='git commit -m'
 alias gco='git checkout'
 alias gcount='git shortlog -sn'
 compdef gcount=git
@@ -58,10 +48,6 @@ alias glp="_git_log_prettily"
 compdef _git glp=git-log
 
 alias gm='git merge'
-alias gmom='git merge origin/master'
-alias gmt='git mergetool --no-prompt'
-alias gmtvim='git mergetool --no-prompt --tool=vimdiff'
-alias gmum='git merge upstream/master'
 
 alias gp='git push'
 alias gpd='git push --dry-run'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,4 @@
-# ======= MOTHER FUCKING ALIAS =======
-# - LS -
-
+# ls
 if [ "$(uname)" == "Darwin" ]; then
   alias ls="ls -G"
 else
@@ -11,53 +9,33 @@ alias l="ls"
 alias ll="ls -lh"
 alias la="ls -a"
 alias l.='ls -d .*'
-alias llol="ls -l | lolcat"
 
-# - Screen -
+# Better defaults
+alias mv="mv -i"
+alias rm="rm -i"
+alias grep="grep --color"
+
+# Screen
 alias sc="screen -S"
 alias sl="screen -ls"
 alias sr="screen -r"
 alias sMemo="echo -e 'sc: create with name\nsl: list\nsr: reattach\nctrl+a d: detach'"
 
-# - Bad Typing -
+# Typos
 alias xs="cd"
 alias vf="cd"
 alias car="cat"
 
-# - Short -
-alias p="pwd"
-alias c="clear"
-alias cls="clear"
-alias mv="mv -i"
-alias rm="rm -i"
+# Short
 alias ..="cd .."
 
-# - Apt -
-alias aga='sudo apt-get autoclean' # aac
-alias agb='sudo apt-get build-dep' # abd
-alias agc='sudo apt-get clean'     # adc
-alias agd='sudo apt-get dselect-upgrade' # ads
-alias agi='sudo apt-get install'  # ai
-alias agp='sudo apt-get purge'    # ap
-alias agr='sudo apt-get remove'   # ar
-alias agu='sudo apt-get update'   # ad
-alias agud='sudo apt-get update && sudo apt-get dist-upgrade' #adu
-alias agug='sudo apt-get upgrade' # ag
-alias aguu='sudo apt-get update && sudo apt-get upgrade'      #adg
-
-# - Fun stuff -
-alias grep="grep --color"
-alias cafe="cat /dev/urandom | hexdump -C | grep 'ca fe'"
-
-# - Vagrant -
+# Vagrant
 alias vv="vagrant up && vagrant ssh"
 alias vh="vagrant halt"
 alias vs="vagrant ssh"
 
-# - GVim -
+# GVim
 #Discard error
 alias gvim="gvim 2>/dev/null"
-alias mem='mvim "+VimwikiIndex" "+VimwikiGoto Mémoire" "+VimwikiGoto Plan" "+Goyo" "+set nospell"'
-alias redac='cd ~/CloudStation/Mémoire/Redaction && mvim document.tex'
 
 alias tmux='tmux -2'
