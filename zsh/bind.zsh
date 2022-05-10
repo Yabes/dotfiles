54e64b5cb009f455d4685e8e73de2a447176732e
Matthis LEMAITRE--COSQUER <matthis.lemaitre-cosquer@niji.fr>
2022-05-10 16:52:49 +0200

    feat: update fzf git preview

--- zsh/bind.zsh
+++ zsh/bind.zsh
@@ -1,14 +1,7 @@
 fzf-git-add() {
-  local gitPreview="git diff HEAD --color-words=\"[[:alnum:]_.]+|[^[:space:]]\" --color --exit-code {2}"
-  local gitDeletedPreview="git show HEAD^:{2}"
-  local colorPreview="pygmentize {2} 2> /dev/null"
-  local imagePreview="[[ \"\$(file -b -i {2})\" =~ \"^image/\" ]] && tiv -w ${COLUMNS} -h ${LINES} {2}"
-  local fallbackPreview="[[ -d {2} ]] && ls -l --color {2} || cat {2}"
-  local previewCmd="[ ! -f {2} ] && ${gitDeletedPreview} || (${imagePreview} || (${gitPreview} && (${colorPreview} || ${fallbackPreview})))"
-
   local file="$(git status --short | \
     grep "^.\S" | \
-    fzf --multi --reverse --bind "?:toggle-preview" --preview-window wrap --preview $previewCmd| \
+    fzf --multi --reverse --bind "?:toggle-preview" --preview-window nowrap --preview "fzf-preview-file {2}"| \
     awk '{ print $2 }' | \
     tr '\n' ' ')"
   local ret=$?
